24c436e7ec6b87109091725dd5534328a8b2d698
Joshua Crum <joshua.crum@gmail.com>
2019-06-16 21:12:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,35 +6,35 @@ class Student
   #  with DB[:conn]
 
   attr_accessor :name, :grade, :id
-  
+
   def initialize(id=nil, name, grade)
     @id = id
     @name = name
     @grade = grade
   end
-  
+
   def self.create_table
-    sql =  <<-SQL 
+    sql =  <<-SQL
       CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
+        id INTEGER PRIMARY KEY,
+        name TEXT,
         grade TEXT
         )
     SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
-  
+
   def save
     if self.id
       self.update
     else
       sql = <<-SQL
-        INSERT INTO students (name, grade) 
+        INSERT INTO students (name, grade)
         VALUES (?, ?)
       SQL
 
@@ -42,7 +42,7 @@ class Student
 
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
-    
+
   end
 
 
